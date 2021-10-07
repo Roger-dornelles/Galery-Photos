9edf47a3c1ce841b7ecfa0ce04158709c57d0f87
Roger Dornelles <68670116+Roger-dornelles@users.noreply.github.com>
2021-10-07 16:14:02 -0300

    Criação de galeria de fotos

---
+++ src/components/PhotoItem/index.tsx
@@ -0,0 +1,25 @@
+import * as C from './styles';
+import * as Photos from '../../services/photos';
+
+
+
+
+type Props = {
+    url: string;
+    name: string;
+    onDelete: (name: string) => void;
+
+}
+
+export const PhotoItem = ({url, name, onDelete}: Props) => {
+
+
+    return (
+
+        <C.Container>
+            <img src={url} alt={name} />
+            {name}
+            <button onClick={()=>onDelete(name)}>Excluir</button>
+        </C.Container>
+    )
+}
---
+++ src/components/PhotoItem/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+    background-color: #3d3F43;
+    border-radius: 10px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+
+    img{
+        max-width: 100%;
+        display: block;
+        margin-bottom: 10px;
+        border-radius: 10px;
+    }
+
+    button{
+        width: 80%;
+        margin-top: 15px;
+        margin-bottom: 5px;
+        cursor: pointer;
+        border: none;
+        outline: none;
+        padding: 5px;
+        border-radius:4px;
+        color: #fff;
+        background-color: #ea3e39;
+    }
+
+
+`;
+
